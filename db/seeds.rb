a2e99fbea292c5719e31609323ab3f32736b7d56
mostafij <mostafij@nascenia.com>
2015-05-21 22:11:30 +0600

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 User.create!([
-  {email: "salayhin@gmail.com", encrypted_password: "$2a$10$ZxvfrHRwgVdDj411WRE1vuMUZMwoYVGd226lWKbE88dkCgnBDeYa6", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 18, current_sign_in_at: "2015-05-21 15:11:31", last_sign_in_at: "2015-05-21 15:11:16", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", type: "User", provider: nil, uid: nil},
-  ])
+                 {email: "salayhin@gmail.com", password: "12345678", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 18, current_sign_in_at: "2015-05-21 15:11:31", last_sign_in_at: "2015-05-21 15:11:16", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", type: "User", provider: nil, uid: nil},
+             ])
 Admin.create!([
-  {email: "mostafij@nascenia.com", encrypted_password: "$2a$10$KFxg2Zqcpfvb4j9jxJUSM.UhiKy8XF7kjwUbK/OSlF3oQ9ziKQM.6", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 37, current_sign_in_at: "2015-05-21 15:41:43", last_sign_in_at: "2015-05-21 15:12:15", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", type: "Admin", provider: nil, uid: nil}
-])
+                  {email: "mostafij@nascenia.com", password: "12345678", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 37, current_sign_in_at: "2015-05-21 15:41:43", last_sign_in_at: "2015-05-21 15:12:15", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", type: "Admin", provider: nil, uid: nil}
+              ])
 Coach.create!([
-  {email: "faruk@nascenia.com", encrypted_password: "$2a$10$MqTVKyKIBqbnFUw2HOBDIuR4DxUMELcJ2Xgum6TXXrIreGZyy/Anm", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 2, current_sign_in_at: "2015-05-21 15:11:55", last_sign_in_at: "2015-05-21 15:08:51", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", type: "Coach", provider: nil, uid: nil}
-])
+                  {email: "faruk@nascenia.com", password: "12345678", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 2, current_sign_in_at: "2015-05-21 15:11:55", last_sign_in_at: "2015-05-21 15:08:51", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", type: "Coach", provider: nil, uid: nil}
+              ])
