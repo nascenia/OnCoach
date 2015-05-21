5a35bcc5c4066aac2b2f8008ef73cf65be0d47b5
mostafij <mostafij@nascenia.com>
2015-05-21 22:23:32 +0600

    Update seed data, facebook but text on center

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 User.create!([
-                 {email: "salayhin@gmail.com", password: "12345678", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 18, current_sign_in_at: "2015-05-21 15:11:31", last_sign_in_at: "2015-05-21 15:11:16", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", type: "User", provider: nil, uid: nil},
+                 {email: "user@oncoach.com", password: "12345678", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 18, current_sign_in_at: "2015-05-21 15:11:31", last_sign_in_at: "2015-05-21 15:11:16", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", type: "User", provider: nil, uid: nil},
              ])
 Admin.create!([
-                  {email: "mostafij@nascenia.com", password: "12345678", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 37, current_sign_in_at: "2015-05-21 15:41:43", last_sign_in_at: "2015-05-21 15:12:15", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", type: "Admin", provider: nil, uid: nil}
+                  {email: "admin@oncoach.com", password: "12345678", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 37, current_sign_in_at: "2015-05-21 15:41:43", last_sign_in_at: "2015-05-21 15:12:15", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", type: "Admin", provider: nil, uid: nil}
               ])
 Coach.create!([
-                  {email: "faruk@nascenia.com", password: "12345678", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 2, current_sign_in_at: "2015-05-21 15:11:55", last_sign_in_at: "2015-05-21 15:08:51", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", type: "Coach", provider: nil, uid: nil}
+                  {email: "coach@oncoach.com", password: "12345678", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 2, current_sign_in_at: "2015-05-21 15:11:55", last_sign_in_at: "2015-05-21 15:08:51", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", type: "Coach", provider: nil, uid: nil}
               ])
