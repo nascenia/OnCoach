7f39480e3bdc015bac8866435c71c09fe7d55ea5
farukhossen <faruk.08.cse@gmail.com>
2015-06-04 14:30:16 +0600

    some changes in leftbar for showing the submenu when collapsed

--- app/assets/javascripts/left_bar.js
+++ app/assets/javascripts/left_bar.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
 
-    return $("li a.expandable").click(function(){
+    $(document).on('click',".sidebar-menu-expanded li a.expandable",function(evt){
 
         if ($(this).parent().find("ul.sub-menu").is(":hidden")){
+            $("ul.menu").hide();
             $(this).parent().find("ul.sub-menu").slideDown(500);
             $(this).find(".glyphicon-chevron-down").addClass("glyphicon-chevron-up").removeClass("glyphicon-chevron-down");
         }else{
@@ -10,5 +11,19 @@ $(document).ready(function(){
             $(this).find(".glyphicon-chevron-up").addClass("glyphicon-chevron-down").removeClass("glyphicon-chevron-up");
 
         }
+        evt.stopPropagation();
+    });
+    $(document).on('mouseover',".sidebar-menu-collapsed li a.expandable",function(evt){
+        if ($(this).parent().find("ul.sub-menu").is(":hidden")){
+            $("ul.sub-menu").hide();
+            $(this).parent().find("ul.sub-menu").show();
+        }
+        evt.stopPropagation();
+    });
+    $('document').on('mouseover','.sidebar-menu-collapsed ul.sub-menu,.sidebar-manu-collapsed .main-manu li',function(evt){
+        evt.stopPropagation();
+    });
+    $(document).mouseover(function(){
+        $('.sidebar-menu-collapsed .sub-menu').hide();
     });
 });
