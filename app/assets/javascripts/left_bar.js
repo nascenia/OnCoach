f6847a945895b7ca4e3f306ca35b178dde3f8f22
farukhossen <faruk.08.cse@gmail.com>
2015-06-04 15:56:52 +0600

    added some changes in leftbar to fix the submenu visit option

--- app/assets/javascripts/left_bar.js
+++ app/assets/javascripts/left_bar.js
@@ -3,12 +3,16 @@ $(document).ready(function(){
     $(document).on('click',".sidebar-menu-expanded li a.expandable",function(evt){
 
         if ($(this).parent().find("ul.sub-menu").is(":hidden")){
-            $("ul.menu").hide();
+            $("ul.sub-menu").hide();
+            $(".main-menu li > a.expandable").removeClass("open");
             $(this).parent().find("ul.sub-menu").slideDown(500);
             $(this).find(".glyphicon-chevron-down").addClass("glyphicon-chevron-up").removeClass("glyphicon-chevron-down");
+            $(this).addClass('open');
+
         }else{
             $(this).parent().find("ul.sub-menu").slideUp(500);
             $(this).find(".glyphicon-chevron-up").addClass("glyphicon-chevron-down").removeClass("glyphicon-chevron-up");
+            $(this).removeClass("open");
 
         }
         evt.stopPropagation();
@@ -16,14 +20,18 @@ $(document).ready(function(){
     $(document).on('mouseover',".sidebar-menu-collapsed li a.expandable",function(evt){
         if ($(this).parent().find("ul.sub-menu").is(":hidden")){
             $("ul.sub-menu").hide();
+            $(".main-menu li > a.expandable").removeClass("open");
             $(this).parent().find("ul.sub-menu").show();
+            $(this).addClass("open");
         }
         evt.stopPropagation();
     });
-    $('document').on('mouseover','.sidebar-menu-collapsed ul.sub-menu,.sidebar-manu-collapsed .main-manu li',function(evt){
+    $('.sidebar-menu-collapsed .sub-menu').mouseover(function(evt){
         evt.stopPropagation();
     });
+
     $(document).mouseover(function(){
         $('.sidebar-menu-collapsed .sub-menu').hide();
+        $(".sidebar-menu-collapsed .main-menu li > a.expandable").removeClass("open");
     });
 });
