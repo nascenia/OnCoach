888d727e03d2a072ac32c4949cece0cb59389137
Khalid <khalid@nascenia.com>
2015-06-18 09:11:29 +0600

    Removed all console errors.

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,24 +1,29 @@
 $(document).ready(function(){
-    Morris.Bar({
-        element: 'weight-bar-chart',
-        data: [
-            { y: '2015-06-01', a: 46  },
-            { y: '2015-05-01', a: 90   }
-        ],
-        xkey: 'y',
-        ykeys: ['a'],
-        labels: ['Weight']
-    });
-    Morris.Bar({
-        element: 'training-bar-chart',
-        data: [
-            { y: '2015-06-01', a: 450  },
-            { y: '2015-05-01', a: 363   }
-        ],
-        xkey: 'y',
-        ykeys: ['a'],
-        labels: ['Calories Burned']
-    });
+
+    if ($('#weight-bar-chart').length > 0) {
+        Morris.Bar({
+            element: 'weight-bar-chart',
+            data: [
+                { y: '2015-06-01', a: 46  },
+                { y: '2015-05-01', a: 90   }
+            ],
+            xkey: 'y',
+            ykeys: ['a'],
+            labels: ['Weight']
+        });
+    }
+    if ($('#training-bar-chart').length > 0) {
+        Morris.Bar({
+            element: 'training-bar-chart',
+            data: [
+                { y: '2015-06-01', a: 450  },
+                { y: '2015-05-01', a: 363   }
+            ],
+            xkey: 'y',
+            ykeys: ['a'],
+            labels: ['Calories Burned']
+        });
+    }
     $('a.widget-toggle').on('click', function(){
         var content = $(this).closest('.widget').find('.widget-content');
         $(content).slideToggle(500);
