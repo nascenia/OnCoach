f25c525305b50672f263a0b183bf38e3266df377
Khalid <khalid@nascenia.com>
2015-06-23 14:28:00 +0600

    Stabilized the layout from huban creative.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery-1.11.1.min
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
