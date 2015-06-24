c244815a99de8434b4512d4dc92e49283c6ac4bb
Khalid <khalid@nascenia.com>
2015-06-24 10:39:42 +0600

    Implemented home page(login page) as before.

---
+++ app/assets/javascripts/login.js
@@ -0,0 +1,25 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery-1.11.1.min
+//= require pace.min
+//= require login/bootstrap.min
+//= require fastclick
+//= require login/less-1.7.5.min
+//= require login/jquery.browser.min
+//= require login/jquery.appear
+//= require login/index
+//= require login/init
+//= require login/owl.carousel.min
+//= require login/jquery.stellar.min
+//= require jquery.magnific-popup.min
+//= require_self
